e6625c384ce242cb7f81399865948d5b689fb9f0
Your Name <you@example.com>
2025-07-18 11:57:28 +0530

    jul18.1

--- Models/User.cs
+++ Models/User.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-
 
 
 namespace UserManagementCrudApp.Models
@@ -10,8 +10,16 @@ namespace UserManagementCrudApp.Models
     public class User
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
         public string Email { get; set; }
-        public string PhoneNumber { get; set; } 
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [Phone(ErrorMessage = "Invalid phone number")]
+        public string PhoneNumber { get; set; }
     }
 }
